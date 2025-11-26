e0643a23577f9bade90c0ce4ca3ea1ca982fadd3
whtpdnd200 <whtpdnd200@gmail.com>
2025-11-26 12:23:27 +0900

    [조세웅] - api 연동 연습 깃 연동

---
+++ src/main/java/com/apiexample/apiexample/api/ApiController.java
@@ -0,0 +1,16 @@
+package com.apiexample.apiexample.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ApiController {
+
+    @ResponseBody
+    @GetMapping("/api/hello")
+    public String hello() {
+        
+        return "hello!!";
+    }
+}
