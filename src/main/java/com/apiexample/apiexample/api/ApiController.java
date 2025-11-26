00cce6f7ee4c8454a830d06c30e98d43ba98da03
whtpdnd200 <whtpdnd200@gmail.com>
2025-11-26 14:10:38 +0900

    no message

--- src/main/java/com/apiexample/apiexample/api/ApiController.java
+++ src/main/java/com/apiexample/apiexample/api/ApiController.java
@@ -1,5 +1,7 @@
 package com.apiexample.apiexample.api;
 
+import com.apiexample.apiexample.api.service.ApiService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,10 +9,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class ApiController {
 
+    @Autowired
+    private ApiService apiService;
+
     @ResponseBody
     @GetMapping("/api/hello")
     public String hello() {
 
         return "hello!!";
+    }
+
+    @ResponseBody
+    @GetMapping("/api/weather")
+    public String getWeather() {
+
+        return apiService.get();
     }
 }
--- src/main/java/com/apiexample/apiexample/api/service/ApiService.java
+++ src/main/java/com/apiexample/apiexample/api/service/ApiService.java
@@ -8,6 +8,26 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Service
 public class ApiService {
 
+    private WebClient webClient = WebClient.builder()
+                                    .baseUrl("https://apihub.kma.go.kr").build();
 
-    
+    public String get() {
+
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme("https")
+                        .path("api/typ02/openApi/VilageFcstInfoService_2.0/getUltraSrtNcst")
+                        .queryParam("authKey", "FscZkVe2TzWHGZFXto81-A")
+                        .queryParam("numOfRows", "10")
+                        .queryParam("pageNo", "1")
+                        .queryParam("dataType", "JSON")
+                        .queryParam("base_date", "20251125")
+                        .queryParam("base_time", "0600")
+                        .queryParam("nx", "55")
+                        .queryParam("ny", "127")
+                        .build()).retrieve()
+                        .bodyToMono(String.class)
+                        .block();
+
+    }
 }
