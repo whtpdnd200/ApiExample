2666c0b8081d63676316e2da7cb08e2707d55e34
A1-18\A1-18 <shong1332@naver.com>
2025-11-26 18:50:35 +0900

    no message

--- src/main/java/com/apiexample/apiexample/api/ApiController.java
+++ src/main/java/com/apiexample/apiexample/api/ApiController.java
@@ -4,8 +4,10 @@ import com.apiexample.apiexample.api.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/api")
 @Controller
 public class ApiController {
 
@@ -13,14 +15,20 @@ public class ApiController {
     private ApiService apiService;
 
     @ResponseBody
-    @GetMapping("/api/hello")
+    @GetMapping("/hello")
     public String hello() {
 
         return "hello!!";
     }
 
+    @GetMapping("/main")
+    public String view() {
+
+        return "/api/apiMain";
+    }
+
     @ResponseBody
-    @GetMapping("/api/weather")
+    @GetMapping("/weather")
     public String getWeather() {
 
         return apiService.get();
--- src/main/java/com/apiexample/apiexample/api/service/ApiService.java
+++ src/main/java/com/apiexample/apiexample/api/service/ApiService.java
@@ -11,21 +11,22 @@ public class ApiService {
     private WebClient webClient = WebClient.builder()
                                     .baseUrl("https://apihub.kma.go.kr").build();
 
+    String key = "FscZkVe2TzWHGZFXto81-A";
     public String get() {
 
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
-                        .scheme("https")
                         .path("api/typ02/openApi/VilageFcstInfoService_2.0/getUltraSrtNcst")
                         .queryParam("authKey", "FscZkVe2TzWHGZFXto81-A")
                         .queryParam("numOfRows", "10")
                         .queryParam("pageNo", "1")
                         .queryParam("dataType", "JSON")
-                        .queryParam("base_date", "20251125")
+                        .queryParam("base_date", "20251126")
                         .queryParam("base_time", "0600")
                         .queryParam("nx", "55")
                         .queryParam("ny", "127")
-                        .build()).retrieve()
+                        .build())
+                        .retrieve()
                         .bodyToMono(String.class)
                         .block();
 
