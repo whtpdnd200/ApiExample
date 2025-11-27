f9a5d5286a7866b9268929a2e224f8b367b26c70
whtpdnd200 <whtpdnd200@gmail.com>
2025-11-27 13:02:09 +0900

    no message

--- src/main/java/com/apiexample/apiexample/api/ApiController.java
+++ src/main/java/com/apiexample/apiexample/api/ApiController.java
@@ -3,9 +3,14 @@ package com.apiexample.apiexample.api;
 import com.apiexample.apiexample.api.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RequestMapping("/api")
 @Controller
@@ -22,7 +27,26 @@ public class ApiController {
     }
 
     @GetMapping("/main")
-    public String view() {
+    public String view(Model model
+                        , @RequestParam(name = "sgg", required = false) String sgg) {
+
+        System.out.println(sgg);
+        List<String> lst = new ArrayList<>();
+        lst.add("서울");
+        lst.add("전라북도");
+        lst.add("전라남도");
+        lst.add("경상북도");
+        lst.add("경상남도");
+
+        model.addAttribute("cityList", lst);
+
+        List<String> sggList = new ArrayList<>();
+        sggList.add("강남구");
+        sggList.add("강동구");
+        sggList.add("강서구");
+        sggList.add("강북구");
+        model.addAttribute("sggList", sggList);
+
 
         return "/api/apiMain";
     }
